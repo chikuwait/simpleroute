317072f612bb72dab21c56dab66210a56a4a598b
chikuwait <chikuwa.1703@gmail.com>
2022-04-08 23:46:25 +0900

    Send ICMP Message

---
+++ go.mod
@@ -0,0 +1,7 @@
+module simpleroute
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20220403103023-749bd193bc2b
+
+require golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
--- simpleroute.go
+++ simpleroute.go
@@ -2,14 +2,101 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"os"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"golang.org/x/net/ipv4"
 )
+
+func sliceAtob(strings []string) ([]byte, error) {
+	bytes := make([]byte, 0, len(strings))
+
+	for _, str := range strings {
+		i, err := strconv.Atoi(str)
+		if err != nil {
+			return bytes, err
+		}
+		bytes = append(bytes, byte(i))
+	}
+
+	return bytes, nil
+}
+
+func calc_checksum(b []byte) uint16 {
+	var sum uint32
+	for i := 0; i < len(b); i += 2 {
+		sum += uint32(b[i]) | (uint32(b[i+1]) << 8)
+	}
+	sum = (sum >> 16) + (sum & 0xffff)
+	sum = (sum >> 16) + (sum & 0xffff)
+	return uint16(^sum)
+}
+
+func make_packet(dst []byte) ([]byte, error) {
+	v4header := ipv4.Header{
+		Version:  4,
+		Len:      20,
+		TotalLen: 30,
+		TTL:      4,
+		Protocol: 1, //ICMP
+		Dst:      net.IPv4(dst[0], dst[1], dst[2], dst[3]),
+	}
+
+	icmp := []byte{
+		8,    //Type ICMP echo message
+		0,    //Code
+		0,    //Checksum
+		0,    //Checksum
+		0,    //Identifier
+		0,    //Identifier
+		0,    //Sequence Number
+		0,    //Sequence Number
+		0xc0, //Data
+		0xDE, //Data
+	}
+	checksum := calc_checksum(icmp)
+	icmp[2] = byte(checksum)
+	icmp[3] = byte(checksum >> 8)
+
+	ippacket, err := v4header.Marshal()
+	if err != nil {
+		return ippacket, err
+	}
+	return append(ippacket, icmp...), nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Arg Err")
 		os.Exit(1)
 	}
-	s_addr := os.Args[1]
-	fmt.Printf("s_addr %s\n",s_addr)
+
+	s_addr, err := sliceAtob(strings.Split(os.Args[1], "."))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("s_addr %d\n", s_addr)
+
+	sfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sock_addr := syscall.SockaddrInet4{
+		Port: 0,
+		Addr: [4]byte{s_addr[0], s_addr[1], s_addr[2], s_addr[3]},
+	}
+	packet, err := make_packet(s_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = syscall.Sendto(sfd, packet, 0, &sock_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
